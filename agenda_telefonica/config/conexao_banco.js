87184a6a2cdb52d1d9e14a8e99ec80bd94e57c3e
anabeayala <44372270+anabeayala@users.noreply.github.com>
2019-12-04 11:15:40 -0400

    Add files via upload

--- agenda_telefonica/config/conexao_banco.js
+++ agenda_telefonica/config/conexao_banco.js
@@ -1,21 +1,58 @@
-var mysql = require('mysql');
-	
-	var connMySQL = function(){
+	var MongoClient= require('mongodb').MongoClient; /*através dessa linha a variável MongoClient está requerendo o método MongoClient */
+	var url= "mongodb://localhost:27017"; /* essa varável url usase para se conectar ao mongo */
 
-		console.log('Conexão com DB foi estabelecida');
+	//create //
+	MongoClient.connect(url, {userNewUrlParser: true }, 
+		function(err, db) { /*o método MongoClient */
+		if(err) {throw err;}
+		var dbo= db.db('banco_app');
 
-		return mysql.createConnection({
-		host:'localhost',
-		user:'root',
-		password:'',
-		database:'portal_noticias'
-		});	
-}
-	
-	module.exports = function()
-	{ 	
-		
-		console.log('O auto load carregou o módulo de conexão co o DB');
-		return connMySQL;
+		var nome= req.body.nome;
+		var foto= req.body.id;
+		var telefone= req.body.telefone;
 
-	}	
+		dbo.collection('contato').save(
+		{
+			nome: nome,
+			foto: id,
+			telefone: telefone
+	}, function(err, result){
+		if (err) {throw err;} /* */
+		console.log('foto inserida');
+	});
+	res.redirect('/');
+});
+
+	// read
+	MongoClient.connect(url, {userNewUrlParser: true }, 
+		function(err, db) { /*o método MongoClient */
+		if(err) {throw err;}
+		var dbo= db.db('banco_app');
+		dbo.collection('contato').find().toArray
+		(function(err, result){
+			res.render('index', {clientes: result});
+		});
+	});
+
+  //update
+  MongoClient.connect(url, {userNewUrlParser: true }, 
+	function(err, db) { /*o método MongoClient */
+	if(err) {throw err;}
+	var dbo= db.db('banco_app');
+	var collection= dbo.collection('banco_app');
+	collection.find().toArray(function(err, result){
+		if(err) { throw err;} 
+		var find= req.body.procurado;
+		var ObjectID= require('mongodb').ObjectID;
+		var o_id= ObjectID(find);
+		collection.updateOne(
+			{_id: o_id}, 
+			{$set: {
+				nome: req.body.nome,
+				sobrenome: req.body.sobrenome}
+			}, function(err, result){
+				if (err) {throw err;}
+				console.log('Documento atualizado');
+			});
+			res.redirect('/');
+	});
--- agenda_telefonica/config/server.js
+++ agenda_telefonica/config/server.js
@@ -1,24 +1,7 @@
-var express = require ('express');
-var consign = require('consign');
-var bodyParser = require('body-parser');
-var expressValidator = require('express-validator');
-
+var express = require('express');
 var app = express();
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.set('views','./app/views');
 
-app.use(bodyParser.urlencoded({extended:true}));
 
-app.use(expressValidator());
-
-consign()
-
-.then('config/dbConnection.js')
-
-.then('app/models')
-
-.then('app/controllers')
-
-.into(app);
-
-module.exports = app;
+module.exports=app; 
